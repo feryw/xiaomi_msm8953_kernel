074f4de79259af7b378fad250cf2b06b5e27e8e6
Fery Wibowo <ferywu@gmail.com>
2018-05-06 23:38:34 +0700

    fix gpio detection

--- drivers/input/fingerprint/goodix/platform.c
+++ drivers/input/fingerprint/goodix/platform.c
@@ -67,12 +67,12 @@ int gf_parse_dts(struct gf_dev *gf_dev)
 	int rc = 0;
 	int i = 0;
 	pr_warn("--------gf_parse_dts start.--------\n");
-	rc = gf3208_request_named_gpio(gf_dev, "goodix, gpio_reset", &gf_dev->reset_gpio);
+	rc = gf3208_request_named_gpio(gf_dev, "goodix,gpio_reset", &gf_dev->reset_gpio);
 	if (rc) {
 		gf_dbg("Failed to request RESET GPIO. rc = %d\n", rc);
 		return -EPERM;
 	}
-	rc = gf3208_request_named_gpio(gf_dev, "goodix, gpio_irq", &gf_dev->irq_gpio);
+	rc = gf3208_request_named_gpio(gf_dev, "goodix,gpio_irq", &gf_dev->irq_gpio);
 	if (rc) {
 		gf_dbg("Failed to request IRQ GPIO. rc = %d\n", rc);
 		return -EPERM;
